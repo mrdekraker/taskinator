0b1644a58bc71ada5204ba86267b99b05c040fbd
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-08-31 11:21:42 -0500

    added dynamic elements to ToDo list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-const buttonEl = document.querySelector('#save-task');
+const formEl = document.querySelector('#task-form');
 const tasksToDoEl = document.querySelector('#tasks-to-do');
 
-const createTaskHandler = function () {
+const createTaskHandler = function (event) {
+    event.preventDefault();
+    const taskNameInput = document.querySelector("input[name='task-name']").value;
+    const taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     const listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task.';
+
+    // create div to hold task info and add to list item
+    const taskInfoEl = document.createElement('div');
+    taskInfoEl.className = 'task-info';
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput}</h3><span class='task-type'>${taskTypeInput}</span>`;
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
